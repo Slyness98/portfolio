bae73a228fc10df9f1cbf561e8dc65d4a6f4e833
Slyness98 <sethlyness@outlook.com>
2020-04-21 13:40:43 -0700

    Commiting locally compiled css from new scss styling structure, featuring our newly tested and operational floated grid system, which supports from full width down to 1/8th width columns per row. Also installed gh-pages as a dev dependancy to create a build production for gh-pages deployment from the gh-pages branch for testing/viewing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-//this file is necessary to the npm script "start". The app can't be run locally without this file. 
-//This file should be unneccessary once the app is in production.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+var ghpages = require('gh-pages');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ghpages.publish('dist', function(err) {});
