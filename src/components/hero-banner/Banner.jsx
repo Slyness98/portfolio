862b6b10b47d4b760b7082c9dcd08f801041f9a7
Slyness98 <sethlyness@outlook.com>
2020-11-13 12:44:08 -0800

    Consolidated the Home page component into two components; Stars and Banner. The Letter class declaration is segregated into its own file under the hero-banner folder for use in the Banner's instantiateLetter() function. Additionally, the speed values for the typewriter have been edited.

---
+++ src/components/hero-banner/Banner.jsx
@@ -0,0 +1,67 @@
+import React, {useCallback, useEffect} from 'react';
+
+import Letter from './letter';
+import Typewriter from '../typewriter/Typewriter';
+
+const Banner = () => {
+
+  const instantiateLetter = (className, content) => {
+    let letter;
+    content === " "
+      ? letter = new Letter(className, '&nbsp;')
+      : letter = new Letter(className, content);
+    return letter;
+  }
+  
+  const wrapSpanAndInject = useCallback((arr) => {
+        arr.forEach((splitWord, idx) => {
+            let count = 0;
+            while(splitWord.length > count) {
+              let injectHere = document.getElementsByClassName(`banner__text__p--${idx+1}`)[0];  
+              let injectedEl = instantiateLetter("banner__text__p__letter", splitWord[count]);   
+              injectHere.appendChild(injectedEl.node);
+              count++;
+            };
+        });
+  }, []);
+  
+  
+  useEffect(() => {
+      const content = ["Salutations!", "My name is", "Seth Lyness.", "I'm a"];
+      wrapSpanAndInject(content);
+  }, [wrapSpanAndInject]);
+    
+  return (
+    <div className="banner__container">
+        <div className="banner__text">	
+          <p className="banner__text__p banner__text__p--1"/> 
+        
+          <div className="banner__block banner__block--1">
+              <p className="banner__text__p banner__text__p--2"/>   
+              <p className="banner__text__p banner__text__p--3"/> 
+          </div> 
+        
+          <div className="banner__block banner__block--2">
+              <p className="banner__text__p banner__text__p--4"/> 
+              <Typewriter 
+                className = {"typewriter-text"}  
+                textArray = {[
+                  " React.JS Specialist", 
+                  " UI/UX Designer", 
+                  " Enthusiastic API Consumer", 
+                  " SASS / CSS Nerd", 
+                  " PHP Dabbler", 
+                  " Node & Express.JS Engineer", 
+                  " Full Stack Web Developer"
+                ]}
+                typingDelay = {80}
+                erasingDelay = {10}
+                newTextDelay = {1250}
+              />
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/space-background/Stars.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//Parallax stars... Sibling divs rendering layers of 1, 2, and 3px stars 
+const Stars = () => {
+    return (
+      <>
+        <div id='stars'></div>
+		<div id='stars2'></div>
+		<div id='stars3'></div>
+      </>
+    );
+};
+
+export default Stars;
